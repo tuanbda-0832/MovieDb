8f766728f18a9d32febde579c6ae12f12075eea6
Duong Tuan <duong.anh.tuanb@sun-asterisk.com>
2019-05-13 09:19:30 +0700

    Create ui movie detail screen

--- app/src/main/java/com/example/moviedb/data/model/Movie.kt
+++ app/src/main/java/com/example/moviedb/data/model/Movie.kt
@@ -16,5 +16,6 @@ data class Movie(
     @SerializedName("backdrop_path") val backdrop_path: String? = null,
     @SerializedName("adult") val adult: Boolean? = null,
     @SerializedName("overview") val overview: String? = null,
-    @SerializedName("release_date") val release_date: String? = null
+    @SerializedName("release_date") val release_date: String? = null,
+    @SerializedName("runtime") val runtime: Int? = null
 )
